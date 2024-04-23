651a28b28af8527068b6ef1f42ef8c7e3bdcfaf4
Suryasmita Swain <suryasmita@asterissolutions.co.in>
2024-04-23 19:38:29 +0530

    Add project files.

---
+++ StudentRegistration/Models/UserPage.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Graph.Models;
+using Newtonsoft.Json;
+
+namespace StudentRegistration.Models
+{
+    public class UserPage
+    {
+        [JsonProperty("@odata.context")]
+        public string ODataContext { get; set; }
+
+        [JsonProperty("value")]
+        public List<User> Value { get; set; }
+
+        [JsonProperty("@odata.nextLink")]
+        public string NextLink { get; set; }
+    }
+}
---
+++ StudentRegistration/Program.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using StudentRegistration.Data;
+using StudentRegistration.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Register DbContext
+var connectionString = builder.Configuration.GetConnectionString("SqlConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+//Register Student Service
+builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+
+
+// Add services to the container.
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAdB2C"));
+
+
+builder.Services.AddControllersWithViews(options =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+    options.Filters.Add(new AuthorizeFilter(policy));
+});
+builder.Services.AddRazorPages()
+    .AddMicrosoftIdentityUI();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
